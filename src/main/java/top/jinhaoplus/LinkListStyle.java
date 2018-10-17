597ddfd6280ffaa0cc81a650881224203b20ee06
JinhaoPlus <rowin2013@gmail.com>
2018-10-17 20:57:39 +0800

    init

---
+++ src/main/java/top/jinhaoplus/LinkListStyle.java
@@ -0,0 +1,33 @@
+package top.jinhaoplus;
+
+public class LinkListStyle {
+
+}
+
+class LinkList {
+    Node head;
+
+    LinkList() {
+        this.head = new Node();
+    }
+
+    void addNode(int value) {
+        Node pointer = head;
+        if (pointer != null) {
+            pointer
+        }
+    }
+}
+
+class Node {
+    int value;
+    Node next;
+
+    Node() {
+    }
+
+    Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
